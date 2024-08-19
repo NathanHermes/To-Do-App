ba181eb355a942fc1d629b222b8ce8eb67847896
NathanHermes <hermesnathan8@gmail.com>
2024-08-19 14:00:53 -0300

    feat: Implement basic To-Do list functionality in Home component
    - Add state management for tasks and new task input
    - Create form for adding new tasks
    - Display list of tasks using unordered list
    - Implement handleSubmit function to add new tasks

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,29 @@
+import { useState } from 'react'
+
 export const Home = () => {
-  return <> <h1>Hello World</h1></>
+  const [ newTask, setNewTask ] = useState( '' )
+  const [ tasks, setTasks ] = useState( Array<string> )
+
+  const handleSubmit = ( event: React.MouseEvent<HTMLButtonElement> ) => {
+    event.preventDefault()
+
+    setTasks( prevState => [ ...prevState, newTask ] )
+  }
+
+  return (
+    <main>
+      <h1>To-Do</h1>
+
+      <form>
+        <input id='task' type='text' onChange={ event => setNewTask( event.target.value ) } />
+        <button type='submit' onClick={ event => handleSubmit( event ) }>Add</button>
+      </form>
+
+      <ul id="ToDoList">
+        { tasks.map( ( task, index ) => {
+          return <li key={ index }>{ task }</li>
+        } ) }
+      </ul>
+    </main>
+  )
 }
