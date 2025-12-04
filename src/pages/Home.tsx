e3b455edb6c0e166e7fbd7d17196d9d71376aab5
NathanTriplos <nathan.hermes@triplos.com.br>
2025-12-03 21:19:06 -0300

    feat(home): integrar gerenciamento de tarefas com API usando axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,87 +1,140 @@
-import { CheckIcon, PlusIcon, TrashIcon } from '@radix-ui/react-icons'
-import clsx from 'clsx'
-import { FormEvent, useState } from 'react'
+import { Task } from "@/api/Task";
+import { CheckIcon, PlusIcon, TrashIcon } from "@radix-ui/react-icons";
+import axios from "axios";
+import clsx from "clsx";
+import { FormEvent, useEffect, useState } from "react";
 
-type Task = {
-  id: number
-  title: string
-  completed: boolean
-}
-
-interface TasksData {
-  tasks: Task[]
-}
+const LOCAL_STORARE = "tasks";
 
 export const Home = () => {
-  const [ title, setTitle ] = useState( '' )
-  const [ tasksData, setTasksData ] = useState<TasksData>()
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-  const createNewTask = ( event: FormEvent ) => {
-    event.preventDefault()
+  const getTasks = async () => {
+    const _tasks = await axios.get("http://localhost:3000/task");
 
-    let _tasks: Task[] = []
-    if ( tasksData !== undefined ) {
-      _tasks = tasksData.tasks;
-    }
+    if (_tasks) setTasks(_tasks.data);
+  };
 
-    _tasks.push( { id: 1, title: 'Test', completed: false } );
-    setTasksData( { tasks: _tasks } );
+  useEffect(() => {
+    getTasks();
+  }, []);
 
-    setTitle( '' );
-  }
-
-  const handleComplete = ( event: React.MouseEvent<HTMLButtonElement>, index: number, task: Task ) => {
+  const handleCreate = (event: FormEvent) => {
     event.preventDefault();
+    const taskId = !tasks ? 1 : tasks.length + 1;
 
-    task.completed = !task.completed;
-    let _tasks: Task[] = []
-    if ( tasksData !== undefined ) {
-      _tasks = tasksData.tasks;
-    }
+    const newTask: Task = {
+      id: taskId,
+      title: title,
+      completed: false,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
 
-    _tasks[ index ] = task;
-    setTasksData( { tasks: _tasks } );
-  }
+    axios.post("http://localhost:3000/task", newTask);
+    getTasks();
+
+    setTitle("");
+  };
+
+  const handleChange = async (value: string, task: Task) => {
+    const updatedTask: Task = {
+      ...task,
+      title: value,
+      updated_at: new Date(),
+    };
+
+    await axios.put(
+      `http://localhost:3000/task/${updatedTask.id}`,
+      updatedTask
+    );
+    getTasks();
+  };
+
+  const handleComplete = async (task: Task) => {
+    const updatedTask: Task = {
+      ...task,
+      completed: !task.completed,
+      updated_at: new Date(),
+    };
+
+    await axios.put(
+      `http://localhost:3000/task/${updatedTask.id}`,
+      updatedTask
+    );
+    getTasks();
+  };
+
+  const handleDelete = async (task: Task) => {
+    await axios.delete(`http://localhost:3000/task/${task.id}`);
+    getTasks();
+  };
 
   return (
     <>
-      <h1 className='text-3xl font-black'>To-Do</h1>
+      <h1 className="text-3xl font-black">To-Do</h1>
 
-      <form onSubmit={ createNewTask } className='w-full h-auto flex items-center justify-center gap-4'>
+      <form
+        onSubmit={(event) => handleCreate(event)}
+        className="w-full h-auto flex items-center justify-center gap-4"
+      >
         <input
-          id='title'
-          type='text'
-          value={ title }
-          onChange={ event => setTitle( event.target.value ) }
-          className='min-h-8 px-2 py-0.5 outline-none focus:ring-1 ring-offset-2 ring-offset-zinc-950 ring-violet-600 bg-zinc-900' />
+          id="title"
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          className="min-h-8 px-2 py-0.5 outline-none focus:ring-1 ring-offset-2 ring-offset-zinc-950 ring-violet-600 bg-zinc-900"
+        />
 
         <button
-          type='submit'
-          className='max-w-8 max-h-8 w-full h-full flex items-center justify-center bg-violet-600'><PlusIcon /></button>
+          type="submit"
+          className="max-w-8 max-h-8 w-full h-full flex items-center justify-center bg-violet-600"
+        >
+          <PlusIcon />
+        </button>
       </form>
 
-      <section className='w-full h-full flex flex-col items-center py-4 gap-1'>
-        { tasksData?.tasks.map( ( task, index ) => {
-          return (
-            <div key={ index } className='w-full flex items-center justify-between p-4 gap-4 bg-zinc-900'>
-              <button
-                onClick={ event => handleComplete( event, index, task ) }
-                className={ clsx( 'max-w-4 max-h-4 w-full h-full flex items-center justify-center', {
-                  'bg-zinc-800': !task.completed,
-                  'bg-green-600': task.completed,
-                } ) }>{ task.completed ? <CheckIcon /> : "" }</button>
+      <section className="w-full h-full flex flex-col items-center py-4 gap-1">
+        {tasks &&
+          tasks.map((task, index) => {
+            return (
+              <div
+                key={index}
+                className="w-full flex items-center justify-between p-4 gap-4 bg-zinc-900"
+              >
+                <button
+                  onClick={() => handleComplete(task)}
+                  className={clsx(
+                    "max-w-4 max-h-4 w-full h-full flex items-center justify-center",
+                    {
+                      "bg-zinc-800": !task.completed,
+                      "bg-green-600": task.completed,
+                    }
+                  )}
+                >
+                  {task.completed ? <CheckIcon /> : ""}
+                </button>
 
-              <input
-                value={ task.title }
-                disabled={ task.completed }
-                className={ clsx( 'w-full outline-none bg-transparent', {
-                  'line-through': task.completed
-                } ) } />
+                <input
+                  value={task.title}
+                  onChange={(event) => handleChange(event.target.value, task)}
+                  disabled={task.completed}
+                  className={clsx("w-full outline-none bg-transparent", {
+                    "line-through": task.completed,
+                  })}
+                />
 
-              <button className='p-2 border border-solid border-red-600'><TrashIcon /></button>
-            </div> )
-        } ) }
+                <button
+                  onClick={() => handleDelete(task)}
+                  className="p-2 border border-solid border-red-600"
+                >
+                  <TrashIcon />
+                </button>
+              </div>
+            );
+          })}
       </section>
     </>
-  )
-}
+  );
+};
